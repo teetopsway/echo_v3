d67b8eecc1cbc51ad27a4daca108b0844e6e4519
teetopSway <[sjhalver@asu.edu]>
2021-03-11 23:11:07 -0500

    SeedPage changed radio buttons put in rows and now write a user preference file to firestore. Added mapScreen.dart Also created function that allows bypass of seed and user profile creation page.

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -4,17 +4,18 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:echo_v3/screens/seedPage.dart';
-
+import 'package:echo_v3/screens/mapScreen.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    CollectionReference userData  = FirebaseFirestore.instance.collection('userData');
+    CollectionReference userData =
+        FirebaseFirestore.instance.collection('userData');
     final FirebaseAuth auth = FirebaseAuth.instance;
     final User user = auth.currentUser;
     var fName;
@@ -22,19 +23,45 @@ class _HomePageState extends State<HomePage> {
     var cellNumber;
     final userId = user.uid;
     final bool initSetup = true;
+    var a4;
 
     Future<void> addUserData() {
       return userData
-        .add({
-          'fName': fName,
-          'lName': lName,
-          'cellNumber': cellNumber,
-          'initSetup': initSetup,
-          'userId': userId,
-      })
+          .add({
+            'fName': fName,
+            'lName': lName,
+            'cellNumber': cellNumber,
+            'initSetup': initSetup,
+            'userId': userId,
+          })
           .then((value) => print("User Added"))
           .catchError((error) => print("Failed to add user: $error"));
     }
+
+    Future<void> checkReg() async {
+      await FirebaseFirestore.instance
+          .collection("userData")
+          .where('userId', isEqualTo: userId)
+          .get()
+          .then((value) {
+        value.docs.forEach((result) {
+          print(userId);
+          print(result.data());
+          var a2 = result.data()["userId"];
+
+          if (userId == a2) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => mapScreen()));
+            print('pass');
+          } else {
+            print(a4);
+            print('fail');
+          }
+        });
+      });
+    }
+
+    checkReg();
 
     return Scaffold(
       backgroundColor: Colors.blue[50],
@@ -42,7 +69,7 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(10),
         child: ListView(
           children: <Widget>[
-            Container (
+            Container(
               child: Text(
                 "Let's Get Started!",
                 style: TextStyle(fontWeight: FontWeight.w800, fontSize: 60.0),
@@ -70,7 +97,6 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(32.0)))),
             ),
-
             TextField(
               keyboardType: TextInputType.emailAddress,
               textAlign: TextAlign.center,
@@ -88,28 +114,25 @@ class _HomePageState extends State<HomePage> {
               borderRadius: BorderRadius.circular(32.0),
               child: MaterialButton(
                 onPressed: () async {
-                  if(fName != null || lName != null || cellNumber != null) {
-
+                  if (fName != null || lName != null || cellNumber != null) {
                     addUserData();
                     Navigator.push(context,
-                        MaterialPageRoute(
-                            builder: (context) => SeedPage()));
+                        MaterialPageRoute(builder: (context) => SeedPage()));
                   } else {
-                      Fluttertoast.showToast(
-                      msg: "Please Fill All Fields",
-                      toastLength: Toast.LENGTH_SHORT,
-                      gravity: ToastGravity.CENTER,
-                      backgroundColor: Colors.blueAccent,
-                      textColor: Colors.white,
-                      fontSize: 16.0);
-                      }
-                  },
+                    Fluttertoast.showToast(
+                        msg: "Please Fill All Fields",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        backgroundColor: Colors.blueAccent,
+                        textColor: Colors.white,
+                        fontSize: 16.0);
+                  }
+                },
                 minWidth: 200.0,
                 height: 45.0,
                 child: Text(
                   "Next...",
-                  style:
-                  TextStyle(fontWeight: FontWeight.w500, fontSize: 30.0),
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30.0),
                 ),
               ),
             ),
@@ -117,6 +140,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
-
   }
 }
--- lib/screens/mapScreen.dart
+++ lib/screens/mapScreen.dart
@@ -3,7 +3,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:echo_v3/screens/mapScreen.dart';
 
 class mapScreen extends StatefulWidget {
   @override
